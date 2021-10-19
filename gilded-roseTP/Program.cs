3cefeafc363aa5b3080ba8c043ac5bbd7ddf3b20
elian fulachier <elian.fulachier@hotmail.fr>
2021-10-19 16:50:49 +0200

    add new project

---
+++ gilded-roseTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gilded_roseTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
