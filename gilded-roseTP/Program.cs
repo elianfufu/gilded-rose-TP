303ebbe7f53a24d1522d6baf04767ba4691f9851
elian fulachier <elian.fulachier@hotmail.fr>
2021-11-09 15:03:20 +0100

    avancement

--- gilded-roseTP/Program.cs
+++ gilded-roseTP/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using gilded_roseTP.models;
+using System;
+using System.Collections.Generic;
 
 namespace gilded_roseTP
 {
@@ -6,7 +8,44 @@ namespace gilded_roseTP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            element el = new element();
+            el.sellIn = 15;
+            el.quality = 15;
+            el.type = "";
+            UpdateQuality(el);
+            Console.WriteLine(el.sellIn);
+        }
+        public static void UpdateQuality (element element)
+        {
+            element.sellIn -= 1;
+            element.quality -= 1;
+            if (element.sellIn < 0)
+            {
+                element.quality -= 2;
+            }
+
+            if (element.quality < 0)
+            {
+                element.quality = 0;
+            }
+
+            if (element.type == "aged Bire")
+            {
+                element.quality += 1;
+            }
+
+            if (element.quality > 50)
+            {
+                element.quality = 50;
+            }
+
+            if (element.type == "sulfuras")
+            {
+                element.sellIn = element.sellIn;
+                element.quality = element.quality;
+            }
+
+
         }
     }
 }
---
+++ gilded-roseTP/models/element.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gilded_roseTP.models
+{
+    public class element
+    {
+        public int sellIn { get; set; }
+        public int quality { get; set; }
+        public string type { get; set; }
+    }
+}
