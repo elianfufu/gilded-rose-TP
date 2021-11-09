6b79e484b9e9c800cebd0934990277aba4684938
elian fulachier <elian.fulachier@hotmail.fr>
2021-11-09 15:49:17 +0100

    add functions

--- gilded-roseTP/Program.cs
+++ gilded-roseTP/Program.cs
@@ -13,39 +13,58 @@ namespace gilded_roseTP
             el.quality = 15;
             el.type = "";
             UpdateQuality(el);
-            Console.WriteLine(el.sellIn);
         }
-        public static void UpdateQuality (element element)
+
+        public static void SulfurasElement(element element)
         {
-            element.sellIn -= 1;
-            element.quality -= 1;
-            if (element.sellIn < 0)
-            {
-                element.quality -= 2;
-            }
-
-            if (element.quality < 0)
-            {
-                element.quality = 0;
-            }
-
-            if (element.type == "aged Bire")
-            {
-                element.quality += 1;
-            }
-
-            if (element.quality > 50)
-            {
-                element.quality = 50;
-            }
-
             if (element.type == "sulfuras")
             {
                 element.sellIn = element.sellIn;
                 element.quality = element.quality;
             }
+        }
 
+        public static void TypeElement(element element)
+        {
+            if (element.type == "aged Bire" || element.type == "backstage passes")
+            {
+                element.quality++;
+                if (element.type == "backstage passes" && element.sellIn < 10)
+                {
+                    element.quality += 2;
+                }
+                if (element.type == "backstage passes" && element.sellIn < 5)
+                {
+                    element.quality += 3;
+                }
+                if (element.type == "backstage passes" && element.sellIn < 0)
+                {
+                    element.quality = 0;
+                }
+            }
+            else
+            {
+                element.sellIn--;
+                element.quality--;
+            }
+        }
 
+        public static void QualityElement(element element)
+        {
+            if (element.quality > 50)
+            {
+                element.quality = 50;
+            }
+            if (element.quality < 0)
+            {
+                element.quality = 0;
+            }
+        }
+        public static void UpdateQuality (element element)
+        {
+            SulfurasElement(element);
+            TypeElement(element);
+            QualityElement(element);
         }
     }
 }
