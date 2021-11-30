c114eee17153e1d6159a5251e2d5a04a72f17655
Antoine Vicart <antoine.vicart@epsi.fr>
2021-11-30 14:22:52 +0100

    test + class shop
    il manque la classe item

---
+++ gilded-roseTP/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gilded_roseTP
+{
+    class Shop
+    {
+        public IList<Item> Items { get; private set; }
+
+        public Shop(IList<Item> items)
+        {
+            this.Items = items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item i in this.Items)
+            {
+                i.SellIn--;
+                i.Quality--;
+            }
+        }
+
+    }
+}
--- gilded-roseTpTest/UnitTest1.cs
+++ gilded-roseTpTest/UnitTest1.cs
@@ -5,9 +5,45 @@ namespace gilded_roseTpTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private List<Item> items = new List<Item>()
         {
+            new Item("Generic Item", 3, 3),
+            new Item("Generic Item", -1, 4)
+        }
+
+
+        private Shop shop;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.shop = new Shop(Item)
+        }
+
+        [TestMethod]
+        public void Should_HaveSellOnItem()
+        {
+            Assert.AreEqual(3, shop, items[0].SellIn);
+        }
+        [TestMethod]
+        public void Should_HaveQualityOnItem()
+        {
+            Assert.AreEqual(3, shop, items[0].Quality);
+        }
+
+        [TestMethod]
+        public void Should_DecreaseQualityAndSellInEveryDay()
+        {
+            shop.UpdateQuality();
+            Assert.AreEqual(2, shop, items[0].Quality);
+            Assert.AreEqual(2, shop, items[0].SellIn);
+        }
+
+        [TestMethod]
+        public void Should_DecreaseTwiceQualityAndSellInEveryDay()
+        {
+            shop.UpdateQuality();
+             Assert.AreEqual(2, shop, items[2].SellIn);
         }
     }
 }
